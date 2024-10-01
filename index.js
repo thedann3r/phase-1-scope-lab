25f4ce17037e925765bba293eb587b8872fff510
dann <austindan168@gmail.com>
2024-10-01 13:19:44 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+console.log(customerName)
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Alice'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob'
+}
